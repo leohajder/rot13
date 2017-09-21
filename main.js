3580ea7b362d38e565915fbc493148e6ef618092
Leo Hajder <leohajder@gmail.com>
2017-09-21 23:53:34 +0200

    Refactor

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
-$("#in").keyup(function() {
-    var input = $("#in").val();
-    if (input === "") {
-        $("#out").text("The output will be generated in real time...");
-    } else {
-        $("#out").text(Rot13(input));
-    }
-});
-
 function Rot13(str) {
     str = str.toUpperCase();
-    var letters = "";
+    var letters = '';
     for (var i = 0; i < str.length; i++) {
         if (65 <= str.charCodeAt(i) || str.charCodeAt(i) >= 90) {
             if (str.charCodeAt(i) + 13 > 90)
                 letters += String.fromCharCode(str.charCodeAt(i) - 13);
-            else letters += String.fromCharCode(str.charCodeAt(i) + 13);
-        } else letters += str[i];
+            else {
+                letters += String.fromCharCode(str.charCodeAt(i) + 13);
+            }
+        } else {
+            letters += str[i];
+        }
     }
+
     return letters;
 }
 
-$(function() {
-    $("#in").keyup();
+$(function () {
+    $('#in').keyup(function () {
+        var input = $('#in').val();
+        if (input === '') {
+            $('#out').text('The output will be generated in real time...');
+        } else {
+            $('#out').text(Rot13(input));
+        }
+    });
 });
