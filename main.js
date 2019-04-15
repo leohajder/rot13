f32f196b2944e01cfc79e16f89df8a0a7a070450
Leo Hajder <leohajder@gmail.com>
2019-04-15 13:08:40 +0200

    Format code

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-function Rot13(str) {
+function rot13(str) {
     str = str.toUpperCase();
     var letters = '';
+    
     for (var i = 0; i < str.length; i++) {
         if (65 <= str.charCodeAt(i) || str.charCodeAt(i) >= 90) {
             if (str.charCodeAt(i) + 13 > 90)
@@ -19,10 +20,11 @@ function Rot13(str) {
 $(function () {
     $('#in').keyup(function () {
         var input = $('#in').val();
-        if (input === '') {
+        
+        if ('' === input) {
             $('#out').text('The output will be generated in real time...');
         } else {
-            $('#out').text(Rot13(input));
+            $('#out').text(rot13(input));
         }
     });
 });
